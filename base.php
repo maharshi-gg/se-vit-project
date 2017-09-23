beec38075b83acc76f217fcbce48df37751a3d05
maharshi-gg <maharshig.parekh2014@gmail.com>
2017-09-24 01:45:12 +0530

    Update base.php

--- base.php
+++ base.php
@@ -20,7 +20,7 @@ try {
   
   $sql = "SELECT 1 from 'users'";
   $val=$db->prepare($sql);
-  if(!$val->execute())
+  if(!$val)
   {
     $sql1 = "CREATE TABLE `users` (`Username` VARCHAR(30) NOT NULL, `Password` VARCHAR(30) NOT NULL, `Email` VARCHAR(40) NOT NULL, `Mobile` INT(10) NOT NULL, `Comments` VARCHAR(100) NOT NULL)";
     $stmt1 = $db->prepare($sql1);
