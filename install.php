46b75c9aaed797f56a8fe8ac5f41d5d808931b9b
maharshi-gg <maharshig.parekh2014@gmail.com>
2017-09-24 15:13:31 +0530

    Update install.php

--- install.php
+++ install.php
@@ -10,7 +10,7 @@ try{
     //$db_conn = substr($url["sep"], 1);
     if(!($db = new mysqli($server,$username,$password))){echo "Error connecting to Db.";}
     $sql=file_get_contents("data/init.sql");
-    $db->exec($sql);
+    $db->query($sql);
    echo "Database and table users created successfully.";
 }
 catch(PDOException $error)
