e97062639f9aa44cabcd92c8e6dd084aec4fc44e
maharshi-gg <maharshig.parekh2014@gmail.com>
2017-09-24 11:11:42 +0530

    Update install.php

--- install.php
+++ install.php
@@ -2,17 +2,16 @@
 //require "config.php";
 try{
     //$connection = new PDO("mysql:host=$host",$username,$password,$options);
-    /*
+    
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $server = $url["127.0.0.1"];
     $username = $url["root"];
     $password = $url[""];
-    $db_conn = substr($url["sep"], 1);
-    if(!($db = new mysqli($server,$username,$password,$db_conn))){echo "Error connecting to Db.";}
+    //$db_conn = substr($url["sep"], 1);
+    if(!($db = new mysqli($server,$username,$password))){echo "Error connecting to Db.";}
     $sql=file_get_contents("data/init.sql");
     $db->exec($sql);
-*/
-    echo "Database and table users created successfully.";
+   echo "Database and table users created successfully.";
 }
 catch(PDOException $error)
 {
